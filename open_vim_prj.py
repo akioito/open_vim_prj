0bcb2226c94edf85b9ad9dc81ec432c8f958e455
aito <itoakya@gmail.com>
2019-07-13 15:14:48 +0900

    minor adjusts

--- open_vim_prj.py
+++ open_vim_prj.py
@@ -10,6 +10,7 @@ class OpenVimPrjCommand(sublime_plugin.WindowCommand):
         active_sheet_view = active_sheet.view()
         current_file = active_sheet_view.file_name()
         if not current_file.endswith('prj'):
+            print('Open in Vim, file must be terminated with "prj"')
             return
         current_dir = dirname(current_file)
         os.chdir(current_dir)
@@ -18,10 +19,8 @@ class OpenVimPrjCommand(sublime_plugin.WindowCommand):
         active_group = window.active_group()
         curr_view_id = window.active_view_in_group(active_group).id()
         for v in window.views_in_group(active_group):
-            if v.id() == curr_view_id:
-                continue
-            window.focus_view(v)
             if not v.file_name().endswith('prj'):
+                window.focus_view(v)
                 window.run_command("close")
 
         file_list_regions = active_sheet_view.split_by_newlines(sublime.Region(0, active_sheet_view.size()))
