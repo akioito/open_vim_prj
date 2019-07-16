5eaa4a2266d7d6ae0cc13f70cc490e4a3ef5d29a
cncloud <cncloud@carenet.co.jp>
2019-07-16 10:56:50 +0900

    adjust project at top of Open Files

--- open_vim_prj.py
+++ open_vim_prj.py
@@ -4,11 +4,12 @@ from glob import glob
 import os
 from os.path import abspath, dirname
 
+
 class OpenVimPrjCommand(sublime_plugin.WindowCommand):
     def run(self):
         active_sheet = self.window.active_sheet()
-        active_sheet_view = active_sheet.view()
-        current_file = active_sheet_view.file_name()
+        active_view = active_sheet.view()
+        current_file = active_view.file_name()
         if not current_file.endswith('prj'):
             print('Open in Vim, file must be terminated with "prj"')
             return
@@ -17,21 +18,28 @@ class OpenVimPrjCommand(sublime_plugin.WindowCommand):
 
         window = self.window
         active_group = window.active_group()
-        curr_view_id = window.active_view_in_group(active_group).id()
-        for v in window.views_in_group(active_group):
-            if v.file_name() and not v.file_name().endswith('prj'):
-                window.focus_view(v)
-                window.run_command("close")
 
-        file_list_regions = active_sheet_view.split_by_newlines(sublime.Region(0, active_sheet_view.size()))
+        prjList = []
+        for v in window.views_in_group(active_group):
+            file_name = v.file_name()
+            if file_name:
+                if file_name != current_file:
+                    if file_name.endswith('prj'):
+                        prjList.append(file_name)
+                    window.focus_view(v)
+                    window.run_command("close")
+        prjList.insert(0, current_file)  # project at top of Open Files
+        file_list_regions = active_view.split_by_newlines(sublime.Region(0, active_view.size()))
         filesList = []
         for region in file_list_regions:
             if (region.size() == 0):
                 continue
-            line = active_sheet_view.substr(region)
+            line = active_view.substr(region)
             if line and not line.startswith('#'):
                 files = glob(line)
                 filesList += files
-        for file in sorted(filesList):
+        window.run_command("close")
+        for file in prjList + sorted(filesList):
             self.window.open_file(abspath(file))
 
+# End
